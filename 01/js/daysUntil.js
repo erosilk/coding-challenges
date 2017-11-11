53805f301b62ac03c049ac2e8b36ae2f680c0c21
Micael Robles <micael.robles@gmail.com>
2017-11-11 09:36:52 -0300

    01 redeemed once again

--- 01/js/daysUntil.js
+++ 01/js/daysUntil.js
@@ -1,12 +1,19 @@
 /* ===== Edit Function ===== */
 
 function daysUntil(date) {
-  const now = moment().dayOfYear();
-  const birthDate = date.dayOfYear();
+  const now = moment().startOf("day");
 
-  return now === birthDate
-    ? "cake"
-    : now > birthDate ? 365 - now + birthDate : birthDate - now;
+  // Si no pongo esto se puede llega a calcular un dia menos al cambiar de año.
+  date.startOf("day");
+
+  // Seteamos el año de la fecha al año actual
+  date.year(now.year());
+
+  // Chequeamos si ya pasó, y le agregamos un año si es necesario.
+  if (date.isBefore(now, "day")) date.add(1, "year");
+
+  // Devolvemos "cake" si es hoy, o la diferencia de días.
+  return now.isSame(date, "day") ? "cake" : date.diff(now, "days");
 }
 
 /* ===== End Function ===== */
